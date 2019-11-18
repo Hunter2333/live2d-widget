55683f2ebc1f24812b1d83bcd58ae67f66119480
Hunter2333 <1249881412@qq.com>
2019-11-18 20:05:34 +0800

    correct url to support jsdelivr

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 //注意：live2d_path参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/Hunter2333/live2d-widget@0.5.1/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/Hunter2333/live2d-widget@0.5.2/";
 //const live2d_path = "/live2d-widget/";
 
 //加载waifu.css
